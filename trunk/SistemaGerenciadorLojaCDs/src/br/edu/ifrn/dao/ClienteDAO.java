a78b3e397c044a57b427131eb6b5b9bb1eb475f6
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-01 15:52:34 +0000

    Atualização F.

--- trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ClienteDAO.java
+++ trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ClienteDAO.java
@@ -5,11 +5,32 @@
 
 package br.edu.ifrn.dao;
 
+import br.edu.ifrn.dominio.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Denis
  */
 public class ClienteDAO {
+
+    public ClienteDAO(){
+
+    }
+
+    public void addCliente(Cliente c){
+        try{
+            Connection conexao = ConnectionFactory.getConnection();
+            String instrucao = "INSERT INTO cliente VALUES(?,?,?,?,?,?,?,?,?,?);";
+            PreparedStatement stm = conexao.prepareStatement(instrucao);
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+
+    }
     
 
 }
