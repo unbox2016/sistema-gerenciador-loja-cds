81ffae9829801e896cf18ca5c1bda31745ddf372
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-01 16:31:22 +0000

    Atualização H.

--- SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ClienteDAO.java
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ClienteDAO.java
@@ -6,9 +6,13 @@
 package br.edu.ifrn.dao;
 
 import br.edu.ifrn.dominio.Cliente;
+import br.edu.ifrn.dominio.Funcionario;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -24,13 +28,25 @@ public class ClienteDAO {
     public void addCliente(Cliente c){
         try{
             Connection conexao = ConnectionFactory.getConnection();
-            String instrucao = "INSERT INTO cliente VALUES(?,?,?,?,?,?,?,?,?,?);";
-            PreparedStatement stm = conexao.prepareStatement(instrucao);
+            String ins = "INSERT INTO cliente VALUES(?,?,?,?,?,?,?,?,?,?);";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+            
+            stm.setString(1, c.getCpf());
+            stm.setInt(2, c.getConta().getCodigo());
+            stm.setString(3, c.getFunc().getLogin());
+            stm.setString(4, c.getRg());
+            stm.setString(5, c.getNome());
+            stm.setString(6, c.getSexo());
+            stm.setString(7, c.getTelefone());
+            stm.setString(8, c.getEstCivil());
+            stm.setDate(9,(Date) c.getDataNasc());
+            stm.executeUpdate();
+            stm.close();
+
         }catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
         }
 
-    }
-    
+    } 
 
 }
