f0af5cc855204aad2038acd832dce70c46527c3e
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-02 16:59:26 +0000

    Atualização D.04.

--- SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/AdministradorDAO.java
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/AdministradorDAO.java
@@ -23,21 +23,20 @@ public class AdministradorDAO {
     }
 
     public void addAdministrador(Administrador adm) {
+        
         try{
-            Connection conexao = ConnectionFactory.getConnection();
-            String ins = "INSERT INTO administrador VALUES(?,?,?,?,?,?,?,?,?,?);";
-            PreparedStatement stm = conexao.prepareStatement(ins);
 
+            Connection conexao = ConnectionFactory.getConnection();
+            String ins = "INSERT INTO administrador VALUES(?);";
+            PreparedStatement stm = conexao.prepareStatement(ins);
             stm.setString(1, adm.getLogin());
-            stm.setString(2, adm.getNome());
-            stm.setString(3, adm.getSenha());
-            stm.setString(4, adm.getCpf());
-            stm.setString(5, adm.getRg());
-            stm.setDate(6,(Date) adm.getDNascimento());
-            stm.setString(7, adm.getSexo());
-            stm.setString(8, adm.getTelefone());
-            stm.setString(9, adm.getLogin());
-            stm.setBoolean(10, adm.isIsAdm());
+
+            FuncionarioDAO fdao = new FuncionarioDAO();
+            fdao.addFuncionario(adm);
+            
+            stm.executeUpdate();
+            conexao.close();
+            
             
         }catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
