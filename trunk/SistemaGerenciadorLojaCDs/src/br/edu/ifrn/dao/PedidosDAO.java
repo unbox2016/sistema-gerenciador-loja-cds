34378dbcda6bd9adc8b2ff0e31aeb7f292240ac4
boyinacio <boyinacio@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-02 14:53:16 +0000

    NOVAS BASES DE DADOS

--- trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/PedidosDAO.java
+++ trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/PedidosDAO.java
@@ -5,10 +5,49 @@
 
 package br.edu.ifrn.dao;
 
+import br.edu.ifrn.dominio.Pedidos;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 2007134010467
  */
 public class PedidosDAO {
+    
+    public void addPedido(Pedidos p){
+        try {
 
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+    }
+
+    public void updatePedido(Pedidos p){
+         try {
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+    }
+
+    public void deletePedido(Pedidos p){
+         try {
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+    }
+
+    LinkedList<Pedidos> listaPedidos = new LinkedList();
+    public LinkedList<Pedidos> selectPedido(){
+         try {
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+
+         return listaPedidos;
+    }
 }
