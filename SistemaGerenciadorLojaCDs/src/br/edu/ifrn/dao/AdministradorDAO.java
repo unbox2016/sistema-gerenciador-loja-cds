93f6d0a0d8eb0d52473c191fd0af29c76616085b
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-02 17:42:46 +0000

    Atualização D.05.

--- SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/AdministradorDAO.java
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/AdministradorDAO.java
@@ -6,10 +6,12 @@
 package br.edu.ifrn.dao;
 
 import br.edu.ifrn.dominio.Administrador;
+import br.edu.ifrn.dominio.Funcionario;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,6 +24,8 @@ public class AdministradorDAO {
 
     }
 
+    private FuncionarioDAO fdao = new FuncionarioDAO();
+
     public void addAdministrador(Administrador adm) {
         
         try{
@@ -31,16 +35,58 @@ public class AdministradorDAO {
             PreparedStatement stm = conexao.prepareStatement(ins);
             stm.setString(1, adm.getLogin());
 
-            FuncionarioDAO fdao = new FuncionarioDAO();
             fdao.addFuncionario(adm);
             
             stm.executeUpdate();
-            conexao.close();
-            
+            conexao.close();         
             
         }catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
         }
+
+    }
+
+    public void updateAdministrador(Administrador adm){
+        fdao.updateFuncionario(adm);
+
+    }
+
+    public void deleteAdministrador(Administrador adm){
+        try {
+            Connection conexao = ConnectionFactory.getConnection();
+            String ins = "DELETE FROM administrador WHERE funcionario_login='?'";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+
+            stm.setString(1, adm.getLogin());
+            fdao.deleteFuncionario(adm);
+
+            stm.executeUpdate();
+            stm.close();
+
+            conexao.close();
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+    }
+
+    public LinkedList<Administrador> selectAdministrador(Administrador adm){
+        LinkedList<Administrador> listaAdm = new LinkedList();
+         try {
+            Connection conexao = ConnectionFactory.getConnection();
+            String ins = "SELECT * FROM funcionario;";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Administrador a = new Administrador(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(4), rs.getString(5), rs.getDate(6), rs.getBoolean(10));
+                listaAdm.add(a);
+            }
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        } finally{
+             return listaAdm;
+        }       
 
     }
 }
