d99c3b000e187545e5f6402cc0e7c3c68a4c2ca9
ericoasisflu <ericoasisflu@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-02 14:57:03 +0000

    Atualização GUIs Update

--- trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/GeneroDAO.java
+++ trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/GeneroDAO.java
@@ -5,10 +5,34 @@
 
 package br.edu.ifrn.dao;
 
+import br.edu.ifrn.dominio.Genero;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 2007134010467
  */
 public class GeneroDAO {
+    
+    public GeneroDAO(){
+        
+    }
 
+    public void addGenero(Genero gen){
+        try{
+            Connection conexao = ConnectionFactory.getConnection();
+            String ins = "INSERT INTO GENERO VALUES(?);";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+        
+            stm.setString(1,gen.getNome());
+            stm.executeUpdate();//Executa atualização
+    }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados." + ex.getMessage());
+        }
+
+    }
 }
+
