c147d62600ec17efd27d3c0e5042cb69b4ad0e77
boyinacio <boyinacio@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-04 22:36:46 +0000

    Atualização Inácio - Tive que alterar o banco de dados de modo que o método selectConta desse certo: eu adicionei a tabela conta o campo CD_ID (chave estrangeira do campo ID de CD). O construtor da classe conta pede um objeto do tipo CD e uma Data. É tanto que nesse metodo terá um "new Date()" no construtor da conta.

--- SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ContaDAO.java
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ContaDAO.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
@@ -23,11 +24,12 @@ public class ContaDAO {
     public void addConta(Conta cont){
         try {
             Connection conexao = ConnectionFactory.getConnection();
-            String ins = "INSERT INTO conta VALUES(?,?);";
+            String ins = "INSERT INTO conta VALUES(?,?,?);";
             PreparedStatement stm = conexao.prepareStatement(ins);
 
             stm.setInt(1, cont.getCodigo());
             stm.setFloat(2, cont.getSaldoTotal());
+            stm.setInt(3, cont.getCDID());
 
             stm.executeUpdate();
             stm.close();
@@ -41,12 +43,13 @@ public class ContaDAO {
     public void updateConta(Conta cont){
         try {
             Connection conexao = ConnectionFactory.getConnection();
-            String ins = "UPDATE conta SET codigo=?, saldototal=? WHERE codigo=?;";
+            String ins = "UPDATE conta SET codigo=?, saldototal=?, cd_id=? WHERE codigo=?;";
             PreparedStatement stm = conexao.prepareStatement(ins);
 
             stm.setInt(1, cont.getCodigo());
             stm.setFloat(2, cont.getSaldoTotal());
-            stm.setInt(3, cont.getCodigo());
+            stm.setInt(3, cont.getCDID());
+            stm.setInt(4, cont.getCodigo());
 
             stm.executeUpdate();
             stm.close();
@@ -80,12 +83,19 @@ public class ContaDAO {
             Connection conexao = ConnectionFactory.getConnection();
             String ins = "SELECT * FROM conta;";
             PreparedStatement stm = conexao.prepareStatement(ins);
+            ResultSet rs = (ResultSet) stm.executeQuery();
 
-            ResultSet rs = (ResultSet) stm.executeQuery();        
+            CDDAO cdDAO = new CDDAO();
+            LinkedList<CD> listaCD = cdDAO.selectCD();
+           
 
             while(rs.next()){
-                Conta conta = new Conta(rs.getInt(1),rs.getFloat(2));
-                listaCon.add(conta);
+                for(CD cd: listaCD){
+                    if(cd.getId() == rs.getInt(3)){
+                        Conta c = new Conta(cd, new Date());
+                        listaCon.add(c);
+                    }
+                }
             }
 
         } catch(SQLException ex){
