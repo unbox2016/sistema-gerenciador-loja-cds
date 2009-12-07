c1762e2cee5e4a6862bc5a7c0011456a39440f54
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-07 14:06:36 +0000

    Atualização D.10.

--- SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ReclamacoesDAO.java
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/dao/ReclamacoesDAO.java
@@ -5,10 +5,13 @@
 
 package br.edu.ifrn.dao;
 
+import br.edu.ifrn.dominio.Cliente;
 import br.edu.ifrn.dominio.Reclamacoes;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -41,6 +44,57 @@ public class ReclamacoesDAO {
         }catch(SQLException ex){
               JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados. \n" + ex.getMessage());
         }
+
+    }
+
+    public void deleteReclamacoes(Reclamacoes r){
+        try{
+            String ins = "DELETE FROM reclamacoes WHERE codigo=?";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+
+            stm.setInt(1, r.getCodigo());
+
+            stm.executeUpdate();
+            stm.close();
+
+            conexao.close();
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados. \n" + ex.getMessage());
+        }
+    }
+
+    public LinkedList<Reclamacoes> selectReclamacoes(){
+        LinkedList<Reclamacoes> listaReclam = new LinkedList();
+        try{
+            String ins = "SELECT * FROM reclamacoes";
+            PreparedStatement stm = conexao.prepareStatement(ins);
+
+            ResultSet rs = stm.executeQuery();
+
+            while(rs.next()){
+                ClienteDAO cdao = new ClienteDAO();
+                LinkedList<Cliente> listaCli = cdao.selectCliente();
+                Cliente cli = null;
+                for(Cliente c:listaCli){
+                    if(rs.getString(2).equals(c.getCpf())){
+                        cli = c;
+                    }
+                }
+
+                Reclamacoes r = new Reclamacoes(rs.getInt(1), cli, rs.getString(3));
+
+            }
+
+            rs.close();
+            stm.close();
+            conexao.close();
+
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao banco de dados. \n" + ex.getMessage());
+        } finally{
+            return listaReclam;
+        }
+
 
     }
 }
