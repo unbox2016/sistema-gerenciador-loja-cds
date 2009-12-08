fbc6109e6b5ec26f9e933a16e86877801a43a8e8
boyinacio <boyinacio@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-08 17:23:11 +0000

    Atualização Inácio

---
+++ SistemaGerenciadorLojaCDs/src/br/edu/ifrn/principal/Executor.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifrn.principal;
+
+import br.edu.ifrn.dao.AdministradorDAO;
+import br.edu.ifrn.dominio.Administrador;
+import br.edu.ifrn.gui.CadastroAdministrador;
+import br.edu.ifrn.gui.TelaLogin;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Executor {
+
+    
+    AdministradorDAO admdao = new AdministradorDAO();
+    LinkedList<Administrador> list;
+    boolean test;
+
+    public Executor() {
+        list = admdao.selectAdministrador();
+        
+        for(Administrador a:list){
+            if(a == null){
+                test = false;
+            }
+            else {
+                test = true;
+                break;
+            }
+        }
+
+        if(test){
+            new TelaLogin().setVisible(true);
+        }
+
+        else {
+            new CadastroAdministrador().setVisible(true);
+        }
+
+    }
+
+}
