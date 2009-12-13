146dfb5e13a564cd4a9b2259e07000f4bd24ed10
dnsricardo <dnsricardo@9421977c-ddaf-11de-af3d-4d7ff200f69e>
2009-12-13 01:23:00 +0000

    Atualização D.19. Conclusão de GerenciarFuncionário.

--- trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/gui/GerenciarFuncionario.java
+++ trunk/SistemaGerenciadorLojaCDs/src/br/edu/ifrn/gui/GerenciarFuncionario.java
@@ -11,6 +11,18 @@
 
 package br.edu.ifrn.gui;
 
+import br.edu.ifrn.dao.AdministradorDAO;
+import br.edu.ifrn.dao.FuncionarioDAO;
+import br.edu.ifrn.dao.VendedorDAO;
+import br.edu.ifrn.dominio.Administrador;
+import br.edu.ifrn.dominio.Funcionario;
+import br.edu.ifrn.dominio.Vendedor;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author 2007134010475
@@ -19,8 +31,13 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
 
     /** Creates new form UpdateFuncionario */
     public GerenciarFuncionario() {
+        fdao = new FuncionarioDAO();
+        listaFunc = fdao.selectFuncionario();
         initComponents();
     }
+
+    LinkedList<Funcionario> listaFunc;
+    FuncionarioDAO fdao;
 
     /** This method is called from within the constructor to
      * initialize the form.
@@ -36,22 +53,22 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         confirmar = new javax.swing.JButton();
-        limpar = new javax.swing.JButton();
+        deletar = new javax.swing.JButton();
         cancelar = new javax.swing.JButton();
 
         editarFuncionarios.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null}
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Login", "Senha", "Nome", "Sexo", "CPF", "RG", "Telefone", "Estado Civil", "Data de nascimento", "Disponível", "Administrador"
+                "Login", "Senha", "Nome", "Sexo", "CPF", "RG", "Telefone", "Estado civil", "Data de nascimento", "Administrador"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Float.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Object.class, java.lang.Boolean.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Float.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Boolean.class
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -63,12 +80,22 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12));
         jLabel1.setText("Edição de funcionários");
 
-        confirmar.setFont(new java.awt.Font("Tahoma", 0, 12));
+        confirmar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         confirmar.setForeground(new java.awt.Color(0, 102, 0));
         confirmar.setText("Confirmar alterações");
+        confirmar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                confirmarActionPerformed(evt);
+            }
+        });
 
-        limpar.setFont(new java.awt.Font("Tahoma", 0, 12));
-        limpar.setText("Deletar");
+        deletar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        deletar.setText("Deletar");
+        deletar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deletarActionPerformed(evt);
+            }
+        });
 
         cancelar.setFont(new java.awt.Font("Tahoma", 0, 12));
         cancelar.setForeground(new java.awt.Color(255, 0, 0));
@@ -87,7 +114,7 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                             .addComponent(confirmar)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(limpar)
+                            .addComponent(deletar)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(cancelar)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 669, Short.MAX_VALUE))
@@ -105,7 +132,7 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cancelar)
-                    .addComponent(limpar)
+                    .addComponent(deletar)
                     .addComponent(confirmar))
                 .addContainerGap())
         );
@@ -113,19 +140,110 @@ public class GerenciarFuncionario extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void deletarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deletarActionPerformed
+        // TODO add your handling code here:
+        int index = editarFuncionarios.getSelectedRow();
+        Funcionario fDel = getFuncionario(index);
+        if(fDel.isIsAdm()){
+            AdministradorDAO adao = new AdministradorDAO();
+            adao.deleteAdministrador((Administrador) fDel);
+        }else{
+            VendedorDAO vdao = new VendedorDAO();
+            vdao.deleteVendedor((Vendedor) fDel);
+        }
+
+        fdao.deleteFuncionario(fDel);
+
+}//GEN-LAST:event_deletarActionPerformed
+
+    private void confirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmarActionPerformed
+        // TODO add your handling code here:
+        for(int i = 0; i < editarFuncionarios.getRowCount(); i++){
+            Funcionario fun = getFuncionario(i);
+            editar(fun);
+        }
+
+        
+    }//GEN-LAST:event_confirmarActionPerformed
+
     /**
     * @param args the command line arguments
     */
+
+   private Funcionario getFuncionario(int index){
+       String login = (String) editarFuncionarios.getValueAt(index, 0);
+       String senha = (String) editarFuncionarios.getValueAt(index, 1);
+       String nome = (String) editarFuncionarios.getValueAt(index, 2);
+       String sexo = (String) editarFuncionarios.getValueAt(index, 3);
+       String telefone = (String) editarFuncionarios.getValueAt(index, 6);
+       String estCivil = (String) editarFuncionarios.getValueAt(index, 7);
+       String cpf = (String) editarFuncionarios.getValueAt(index, 4);
+       String rg = (String) editarFuncionarios.getValueAt(index, 5);
+       Date dataNasc = (Date) editarFuncionarios.getValueAt(index, 8);
+       boolean adm = (Boolean) editarFuncionarios.getValueAt(index, 9);
+
+       boolean corretos = true;
+
+       try{
+           Float.parseFloat(rg);
+           Float.parseFloat(cpf);
+           Float.parseFloat(telefone);
+       }catch(NumberFormatException ex){
+           corretos = false;
+       }
+
+       if(corretos)
+            return new Funcionario(login, senha, nome, cpf, rg, dataNasc, sexo, telefone, estCivil, adm);
+       else{
+           JOptionPane.showMessageDialog(null, "Por favor, digite apenas números para CPF, RG ou Telefone.");
+           return null;
+       }
+
+   }
+   
+   private void editar(Funcionario f){
+       for(Funcionario func: listaFunc){
+            if(!func.equals(f)){
+               fdao.updateFuncionario(f);
+            }
+
+       }
+   }
+
+   private void generateTable(){
+        Object[][] tableFunc = new Object[listaFunc.size()][10];
+        int pos = 0;
+        String colunas[] = {"Login", "Senha","Nome","Sexo","CPF","RG","Telefone","Estado civil","Data de nascimento","Administrador"};
+        Iterator<Funcionario> it = listaFunc.iterator();
+
+        while(it.hasNext()){
+            Funcionario func = it.next();
+
+            tableFunc[pos][0] = func.getLogin();
+            tableFunc[pos][1] = func.getSenha();
+            tableFunc[pos][2] = func.getNome();
+            tableFunc[pos][3] = func.getSexo();
+            tableFunc[pos][4] = func.getCpf();
+            tableFunc[pos][5] = func.getRg();
+            tableFunc[pos][6] = func.getTelefone();
+            tableFunc[pos][7] = func.getEstCivil();
+            tableFunc[pos][8] = func.getDNascimento();
+            tableFunc[pos][9] = func.isIsAdm();
+
+            pos++;
+        }
+        editarFuncionarios.setModel(new DefaultTableModel(tableFunc, colunas));
+   }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cancelar;
     private javax.swing.JButton confirmar;
+    private javax.swing.JButton deletar;
     private javax.swing.JTable editarFuncionarios;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JButton limpar;
     // End of variables declaration//GEN-END:variables
 
 }
